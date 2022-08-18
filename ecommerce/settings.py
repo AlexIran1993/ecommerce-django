a14a4cf35b8f6bdadb54b05c335e6f34f0e830da
unknown <alexiran1993@gmail.com>
2022-08-17 21:10:04 -0500

    Actualizacion Deploy Heroku 17/08/22 09:09

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -109,28 +109,28 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 #Modelo de autenticacion para que reconozca a Account como clase principal para el almacenamiento de usuarios.
 AUTH_USER_MODEL = 'accounts.Account'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbecommerce',
-        'HOST': 'localhost',
-        'USER': 'ecommerceuseradmin',
-        'PASSWORD': 'ecommerce_admin',
-        'PORT': 5432,
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'dbecommerce',
+#        'HOST': 'localhost',
+#        'USER': 'ecommerceuseradmin',
+#        'PASSWORD': 'ecommerce_admin',
+#        'PORT': 5432,
+#    }
+#}
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -170,10 +170,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join('ecommerce/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 #Archivo que ayudara a manejar los archivos estaticos
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 #Direccion donde se almacenaran los archivos mediadile
