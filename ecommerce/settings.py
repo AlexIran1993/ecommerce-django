861f5e403962c0eb3fecdcc9930c7c0d05b73122
unknown <alexiran1993@gmail.com>
2022-08-17 21:13:59 -0500

    Actualizacion Deploy Heroku 17/08/22 09:13

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -109,28 +109,28 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 #Modelo de autenticacion para que reconozca a Account como clase principal para el almacenamiento de usuarios.
 AUTH_USER_MODEL = 'accounts.Account'
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'dbecommerce',
-#        'HOST': 'localhost',
-#        'USER': 'ecommerceuseradmin',
-#        'PASSWORD': 'ecommerce_admin',
-#        'PORT': 5432,
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbecommerce',
+        'HOST': 'localhost',
+        'USER': 'ecommerceuseradmin',
+        'PASSWORD': 'ecommerce_admin',
+        'PORT': 5432,
+    }
+}
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
