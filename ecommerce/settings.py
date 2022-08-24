48a6ab926c5737c43f271bcfb3de321ec5b2d604
unknown <alexiran1993@gmail.com>
2022-08-24 00:36:24 -0500

    Actualizacion

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -171,7 +171,7 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'ecommerce/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
